7c5b030eec13e82d03fb85afc654140732ca9c9c
josecsh <tito789551>
2018-07-17 20:53:26 +0200

    NoticiasAPP funcionando. Faltan detalles de interface

--- app/src/main/java/com/example/jose_pc/noticiasapp/Geolocation.java
+++ app/src/main/java/com/example/jose_pc/noticiasapp/Geolocation.java
@@ -1,42 +1,327 @@
 package com.example.jose_pc.noticiasapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import butterknife.ButterKnife;
+
 public class Geolocation extends AppCompatActivity {
 
+    private static final String TAG = Geolocation.class.getSimpleName();
+
     private double latitude, longitude;
-    String loc;
+    private String localidad;
+    private String cp;
+    private Boolean onGranada = false;
+
+    // location last updated time
+    private String mLastUpdateTime;
+
+    // location updates interval - 10sec
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+    // fastest updates interval - 5 sec
+    // location updates will be received if another app is requesting the locations
+    // than your app can handle
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
+
+    private static final int REQUEST_CHECK_SETTINGS = 100;
+
+
+    // bunch of location related apis
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+
+    // boolean flag to toggle the ui
+    private Boolean mRequestingLocationUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.geolocation_wait);
 
-        // https://github.com/eddydn/GetAddressGeocode
+        // VER QUE HACE ESTO
+        ButterKnife.bind(this);
 
-        // Casa
-        //latitude = 37.163894;
-        //longitude = -3.596656;
+        // Inicializa las librerias necesarias
+        init();
 
-        // Apartamento playa
-        //latitude = 36.732444;
-        //longitude = -3.682693;
+        startLocationPermission();
 
-        // Armilla
-        //latitude = 37.142772;
-        //longitude = -3.627786;
+    }
 
-        // Casa Lorena
-        latitude = 37.190968;
-        longitude = -3.624540;
+    private void init() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                // location is received
+                mCurrentLocation = locationResult.getLastLocation();
+                mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+                updateLocationUI();
+            }
+        };
+
+        mRequestingLocationUpdates = false;
+
+
+        // --------------------------------------------------------------------------------
+        // ----------- Probar a quitar el intervalo cuando tenga servidor -----------------
+        // --------------------------------------------------------------------------------
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    private void updateLocationUI() {
+        if (mCurrentLocation != null) {
+
+            //latitude = mCurrentLocation.getLatitude();
+            //longitude = mCurrentLocation.getLongitude();
+
+            // Casa (zaidin)
+            //latitude = 37.161946;
+            //longitude = -3.594356;
+
+            // Apartamento playa
+            //latitude = 36.732522;
+            //longitude = -3.682933;
+
+            // Motril
+            //latitude = 37.744335;
+            //longitude = -3.523801;
+
+            // Beiro
+            //latitude=37.193017;
+            //longitude=-3.600814;
+
+            // Sevilla
+            //latitude= 37.386108;
+            //longitude= -5.991456;
+
+            // Churriana de la Vega
+            latitude = 37.145926;
+            longitude = -3.646057;
+
+            // Casa Lorena (chana)
+            //latitude = 37.190968;
+            //longitude = -3.624540;
+
+            stopLocationUpdates();
+
+            getLocation();
+        }
+    }
+
+
+    private void startLocationUpdates() {
+        mSettingsClient
+                .checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i(TAG, "All location settings are satisfied.");
+
+                        //noinspection MissingPermission
+                        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                mLocationCallback, Looper.myLooper());
+
+                        updateLocationUI();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(Geolocation.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e(TAG, errorMessage);
+
+                                Toast.makeText(Geolocation.this, errorMessage, Toast.LENGTH_LONG).show();
+                        }
+
+                        updateLocationUI();
+                    }
+                });
+    }
+
+    public void startLocationPermission() {
+        // Requesting ACCESS_FINE_LOCATION using Dexter library
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        mRequestingLocationUpdates = true;
+                        startLocationUpdates();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            // open device settings when the permission is
+                            // denied permanently
+                            openSettings();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public void stopLocationUpdates() {
+        // Removing location updates
+        mRequestingLocationUpdates = false;
+        mFusedLocationClient
+                .removeLocationUpdates(mLocationCallback)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.e(TAG, "User agreed to make required location settings changes.");
+                        // Nothing to do. startLocationupdates() gets called in onResume again.
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.e(TAG, "User chose not to make required location settings changes.");
+                        mRequestingLocationUpdates = false;
+
+                        // Si el usuario elige que no, vuelve a la pantalla de noticias sin geolocalización
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        finish();
+                        startActivity(i);
+                        break;
+                }
+                break;
+        }
+    }
+
+
+    private void openSettings() {
+        Intent intent = new Intent();
+        intent.setAction(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package",
+                BuildConfig.APPLICATION_ID, null);
+        intent.setData(uri);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Resuming location updates depending on button state and
+        // allowed permissions
+        if (mRequestingLocationUpdates && checkPermissions()) {
+            startLocationUpdates();
+        }
+
+        updateLocationUI();
+    }
+
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+
+    private void getLocation (){
+
+        String cpGranada[] = {"18001", "18002",  "18003", "18004", "18005", "18006", "18007",
+                "18008", "18009", "18010", "18011", "18012", "18013", "18014", "18015"};
 
         Geocoder gc = new Geocoder(this, Locale.getDefault());
 
@@ -44,21 +329,29 @@ public class Geolocation extends AppCompatActivity {
             List<Address> list = gc.getFromLocation(latitude, longitude, 1);
             if(list.size() > 0) {
                 Address address = list.get(0);
-                loc = address.getPostalCode();
+                cp = address.getPostalCode();
+                localidad = address.getLocality();
+
+                for(int i=0; i<cpGranada.length; ++i){
+                    if(cp.equals(cpGranada[i])){
+                        onGranada = true;
+                    }
+                }
             }
 
             Intent i = new Intent(getApplicationContext(), GeoListActivity.class);
 
-            //i.putExtra("latitud", latitude);
-            //i.putExtra("longitud", longitude);
-            i.putExtra("ciudad", loc);
+            i.putExtra("en_granada", onGranada);
+            i.putExtra("latitud", latitude);
+            i.putExtra("longitud", longitude);
+            i.putExtra("localidad", localidad);
 
             startActivity(i);
+            finish();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
